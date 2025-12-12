df9852f56b8bc87bfb9ff36fbe4b480b248cb015
AckerWhat <nicolaamoruso1015@gmail.com>
2025-12-12 01:09:23 -0400

    Improve: Update Maps link and enhance mobile responsive design for contact cards

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ export const CONTACT_INFO = {
     whatsapp: "#"
   },
   map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.054910231718!2d-68.0080744!3d10.1761923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e80670048a8bbaf%3A0x4f9acd15aab9ead4!2sTaller%20Automotriz%20Amoruso%20Inversiones%20C.A!5e0!3m2!1sen!2sus!4v1747336906407!5m2!1sen!2sus",
-  mapDirections: "https://www.google.com/maps/dir/?api=1&destination=Taller+Automotriz+Amoruso+Inversiones+C.A,+Calle+Cantaura,+Valencia,+Carabobo,+Venezuela"
+  mapDirections: "https://maps.app.goo.gl/X4vaizs3ev5Q7Tmw7"
 };
 
 export const COMPANY_INFO = {
